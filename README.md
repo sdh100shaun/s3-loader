00cb0f6cad9a20ae0f63049563b56849a66a2b48
shaun hare <shaun@shaunhare.co.uk>
2017-10-07 17:57:32 +0100

    initial load

---
+++ README.md
@@ -0,0 +1,13 @@
+#s3 loader
+
+[![Build Status](https://travis-ci.org/sdh100shaun/MeetupCache.svg?branch=develop)](https://travis-ci.org/sdh100shaun/MeetupCache)
+
+A package to provide a compnent to load a s3 file or directory at build time to get automatically included in a specified location.
+
+Authored by Shaun Hare (c) 2017 
+
+**Usage**
+
+
+
+
---
+++ src/AwsClient.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *
+ * @author Shaun Hare
+ */
+
+namespace ShaunHare\loader;
+
+use Aws\Sdk;
+
+class AwsClient extends Sdk
+{
+
+    /**
+     * @var array
+     */
+    private $bucketDetails;
+
+    public function __construct(array $bucketDetails, array $args = [])
+    {
+        parent::__construct($args);
+        $this->bucketDetails = $bucketDetails;
+    }
+
+
+
+    public function storeToFileFromS3($filename)
+    {
+        /**
+         * Aws/S3/S3Client
+         */
+        $s3 = $this->createS3($this->bucketDetails);
+
+        $s3->getObject([
+            'Bucket' =>$this->bucketDetails['bucket'] ,
+            'Key' => $this->bucketDetails['key'] ,
+            'SaveAs' => $filename
+        ]);
+    }
+}
