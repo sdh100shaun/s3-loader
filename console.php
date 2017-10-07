03a443c2f501b0b58dfbd396cb43240e5dedecad
shaun hare <shaun@shaunhare.co.uk>
2017-10-07 23:02:27 +0100

    starting cli command

--- console.php
+++ console.php
@@ -6,5 +6,5 @@ use Dotenv\Dotenv;
 require __DIR__ . '/vendor/autoload.php';
 (new Dotenv(__DIR__))->load();
 $app = new \Symfony\Component\Console\Application;
-//$app->add(new \App\Command\GreeterCommand);
+$app->add(new ShaunHare\loader\GrabCommand());
 $app->run();
---
+++ src/GrabCommand.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaunhare
+ * Date: 07/10/2017
+ * Time: 22:52
+ */
+
+namespace ShaunHare\loader;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+
+class GrabCommand extends Command
+{
+    public function configure()
+    {
+        $this->setName('grab')
+            ->setDescription('It grabs the file(s) from s3');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->writeln("Starting S3 bucket file retrieval");
+
+    }
+}
